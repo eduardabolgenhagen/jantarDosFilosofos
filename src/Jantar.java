60c11e8a731d0820df9ea4c6a73ef9c172a810e1
eduardabolgenhagen <bolgenhagene4@gmail.com>
2022-09-14 16:10:47 -0300

    :+1:

--- src/Jantar.java
+++ src/Jantar.java
@@ -3,10 +3,9 @@ public class Jantar {
     public static void main(String[] args) {
         //criando uma mesa
         Mesa mesa = new Mesa();
-        //iniciando uma estrutura de repetição e criando os filosofos
+        //iniciando uma estrutura de repetição e criando os filosofos para o Jantar
         for (int filosofo = 0; filosofo < 5; filosofo++) {
             new Filosofo("Filosofo " + filosofo, mesa, filosofo).start();
-            //conferir parametros
         }
     }
 }
